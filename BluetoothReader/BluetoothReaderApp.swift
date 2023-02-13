b4b6febd243e61265bb6c25f97302e1a519b706f
Beau Nouvelle <beau@getswifty.dev>
2023-02-14 08:40:11 +1100

    Initial Commit

---
+++ BluetoothReader/BluetoothReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothReaderApp.swift
+//  BluetoothReader
+//
+//  Created by Beau Nouvelle on 14/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
